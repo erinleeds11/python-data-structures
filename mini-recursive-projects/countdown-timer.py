fcff7de8bb40e2a268a813c7911fcb28b6c8bea8
Erin Leeds <erinleeds@Erins-MacBook-Air.local>
2020-10-20 17:29:33 -0700

    mini-recursive

--- mini-recursive-projects/countdown-timer.py
+++ mini-recursive-projects/countdown-timer.py
@@ -7,4 +7,10 @@ def iter_countdown_timer(n):
         n-=1
     print(n)
 
-    
+def recursive_countdown(n):
+    if n < 0:
+        return
+    print(n)
+    time.sleep(1)
+    return recursive_countdown(n-1)
+recursive_countdown(6)
