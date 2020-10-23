9ccbcbd1999e0e6232815e7d4104f8524f37c609
Erin Leeds <erinleeds@Erins-Air.hsd1.ca.comcast.net>
2020-10-22 17:14:30 -0700

    set_val method

--- bisection-search-proj/hash-project/hash-blueprint.py
+++ bisection-search-proj/hash-project/hash-blueprint.py
@@ -3,16 +3,31 @@ class AlgoHashTable:
     def __init___(self, size):
         self.size = size
         #create many mini lists within one big list
-        self.hash_table = [[] for _ in range(self.size)]
+        self.hash_table = self.create_buckets()
 
     def create_buckets(self):
-        pass
+        return [[] for _ in range(self.size)]
 
     def set_val(self, key, value):
-        pass
+        hashed_key = hash(key)%self.size
+        bucket = self.hash_table[hashed_key] 
+        found_key = False
+        #setting key to some other value
+        
+        for index, record in enumerate(bucket):
+            record_key, record_value = record
+            if record_key == key:
+                found_key = True
+                break
+        if found_key:
+            bucket[index] = (key, value)
+        else:
+
+            bucket.append((key,value)) #append to take care of collision
+
 
     def get_val(self, key):
-        pass
+        
 
     #called when tried to print ht object
     def __str__(self):
